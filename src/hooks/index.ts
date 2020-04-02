64a972a73bd6558455ac5ae52e6395160d55facc
Patrick Dias <pdcamargo@hotmail.com>
2020-04-01 23:06:39 -0300

    tsconfig prod to lib and reorder hooks index file

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,7 @@
-export { default as useIsMobile } from './useIsMobile';
 export { default as useBrowserDetection } from './useBrowserDetection';
+export { default as useClickAway } from './useClickAway';
 export { default as useEventEmitter, EventEmitter } from './useEventEmitter';
 export { default as useInViewport } from './useInViewport';
-export { default as useClickAway } from './useClickAway';
+export { default as useIsMobile } from './useIsMobile';
 export { default as useKeyPress } from './useKeyPress';
 export { default as useSlug } from './useSlug';
