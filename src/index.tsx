3e7f97564ac3cd542eae4da98ae4b0f2558d5836
Patrick Dias <pdcamargo@hotmail.com>
2020-04-13 00:00:37 -0300

    Exporting utils, types and constants in root file

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import { Flex, CssReset, Accordion, AccordionGroup } from './components';
-
 export * from './hooks';
 export * from './components';
-
-function App() {
-  return (
-    <Flex padding="30px">
-      <AccordionGroup>
-        <Accordion title="1 hi" content="hello world" />
-        <Accordion title="2 hi" content="hello world" />
-        <Accordion title="3 hi" content="hello world" />
-      </AccordionGroup>
-      <CssReset />
-    </Flex>
-  );
-}
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+export * from './constants';
+export * from './types';
+export * from './utils';
