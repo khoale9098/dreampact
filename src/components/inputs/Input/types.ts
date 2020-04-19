27b022cb7ec0b941bb5fd6cbd58edfb014b5149e
Patrick Dias <pdcamargo@hotmail.com>
2020-04-18 21:41:11 -0300

    Fix input props

--- src/components/inputs/Input/types.ts
+++ src/components/inputs/Input/types.ts
@@ -5,7 +5,4 @@ export type InputStyleProps = DefaultStyleProps;
 export interface InputProps
   extends Omit<DefaultElementProps<HTMLInputElement>, 'children'>,
     Omit<InputStyleProps, 'theme'>,
-    React.InputHTMLAttributes<HTMLInputElement> {
-  title: string;
-  content: string | JSX.Element;
-}
+    React.InputHTMLAttributes<HTMLInputElement> {}
